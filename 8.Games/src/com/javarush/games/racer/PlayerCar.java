b4ff0f589056c599e663086772c8314d5df9f123
Aleksei Os <oal.aleos@gmail.com>
2023-04-11 12:21:35 +0300

    11.04.2023

--- 8.Games/src/com/javarush/games/racer/PlayerCar.java
+++ 8.Games/src/com/javarush/games/racer/PlayerCar.java
@@ -1,10 +1,35 @@
 package com.javarush.games.racer;
 
+import com.javarush.games.racer.road.RoadManager;
+
 public class PlayerCar extends GameObject {
     private static int playerCarHeight = ShapeMatrix.PLAYER.length;
+    private Direction direction;
     public int speed = 1;
 
     public PlayerCar () {
         super(RacerGame.WIDTH / 2 + 2, RacerGame.HEIGHT - playerCarHeight - 1, ShapeMatrix.PLAYER);
+    }
+
+    public void move() {
+        if (x < RoadManager.LEFT_BORDER) {
+            x = RoadManager.LEFT_BORDER;
+        } else if (x > RoadManager.RIGHT_BORDER - width) {
+                x = RoadManager.RIGHT_BORDER - width;
+        }
+
+        if (direction == Direction.LEFT) {
+                x--;
+        } else if (direction == Direction.RIGHT) {
+                x++;
+        }
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
     }
 }
