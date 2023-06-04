2bf5d77f597ea018b5838abeead2062bd3f453b1
Aleksei Os <oal.aleos@gmail.com>
2023-06-04 12:02:19 +0300

    04.06.2023

---
+++ 2.JavaCore/src/ua/javarush/task/jdk13/task23/task2301/Solution.java
@@ -0,0 +1,28 @@
+package ua.javarush.task.jdk13.task23.task2301;
+
+/* 
+Inner
+*/
+
+public class Solution {
+    public InnerClass[] innerClasses = new InnerClass[2];
+
+    public static void main(String[] args) {
+
+    }
+
+    public static Solution[] getTwoSolutions() {
+
+        Solution[] sol = new Solution[2];
+
+        sol[0].innerClasses[0] = new InnerClass();
+        sol[0].innerClasses[1] = new InnerClass();
+        sol[1].innerClasses[0] = new InnerClass();
+        sol[1].innerClasses[1] = new InnerClass();
+
+        return sol;
+    }
+
+    public static class InnerClass {
+    }
+}
